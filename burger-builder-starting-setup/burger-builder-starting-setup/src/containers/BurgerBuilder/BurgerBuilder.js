ec6c7a85d61aaf13c400d0cca5b4dd4fb7e78bef
mburbage <michael@redcircle.biz>
2020-09-18 17:07:25 -0400

    Webpack

--- burger-builder-starting-setup/burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-starting-setup/burger-builder-starting-setup/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,7 @@ import * as burgerBuilderAction from '../../store/actions/index';
 
 
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
 	state = {
 		purchasing: false,
 	};
